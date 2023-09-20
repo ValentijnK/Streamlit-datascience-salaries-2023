8fa71f2b3138091675e9a8a6f98c9f628c5a4a58
Valentijn Kilian <valentijnkilian@gmail.com>
2023-09-20 14:50:35 +0200

    added streamlit

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # Imports
 import pandas as pd
+import streamlit as st
+
+# Setup Streamlit app
+st.title('Salary of datascientist in 2023')
+
+
+
 
 # Load CSV file into DataFrame
 df = pd.read_csv('ds_salaries.csv')
