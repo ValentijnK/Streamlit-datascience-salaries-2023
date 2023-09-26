cc4f5647756b1438819e22ece1a925d69cae1e05
Valentijn Kilian <valentijnkilian@gmail.com>
2023-09-26 21:36:30 +0200

    Removed redundant import calls

--- pages/Insights.py
+++ pages/Insights.py
@@ -1,9 +1,6 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns
-import matplotlib.pyplot as plt
 import plotly.express as px
-
 
 # Setting page configuration
 st.set_page_config(
